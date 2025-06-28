94b99545137ced8334cde6c640bb1da1616602d3
Piyush Sharma <piyu22551@gmail.com>
2025-06-29 03:04:38 +0530

    removing

--- android/app/src/main/kotlin/com/example/free/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.free
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/pages/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-const Color kPrimaryBlue = Color(0xFF6495ED);
-const Color kAccentBlue = Color(0xFF1E90FF);
-const Color kBackgroundBlue = Color(0xFFF0F8FF);
-const Color kWhite = Color(0xFFFFFFFF);
-const Color kSoftGrey = Color(0xFFE5E5E5);
-const Color kTextDark = Color(0xFF333333);
-const Color kLikeRed = Color(0xFFF08080);
